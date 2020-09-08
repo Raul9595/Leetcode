6a168a9e64101e22a86abfb93f7c6d80cb8b77a7
raul9595 <rahulsvarm@gmail.com>
2020-09-07 22:15:29 -0700

    Create 682.baseball-game.py

---
+++ 682.baseball-game.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=682 lang=python3
+#
+# [682] Baseball Game
+#
+
+# @lc code=start
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        res = []
+        for i in ops:
+            if i == '+':
+                res.append(res[-1] + res[-2])
+            elif i == 'D':
+                res.append(res[-1]*2)
+            elif i == 'C':
+                res.pop()
+            else:
+                res.append(int(i))
+
+        return sum(res)
+# @lc code=end
+
